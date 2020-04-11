85fe8e622367cb8442df6e2a5ff7cb704f8cf91f
Dymytrii Ishchuk <39443782+b4rracuda@users.noreply.github.com>
2020-04-11 18:36:17 +0300

    configuration ready

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
+const config = require('./config.json')
 const Telegraf = require('telegraf')
-
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(config.bot_token)
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
