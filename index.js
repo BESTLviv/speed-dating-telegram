2a804e4a54230feacad2a2b6e9e5ccc2c466a3e4
Oleksii Boiko <olexiy.bojko@gmail.com>
2020-04-22 18:22:59 +0300

    improve  generate_pairs method

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function check_chat_admin(userid, chatid) {
 	
 }
 
-function generate_pairs(members = []) {
-    if(members%2!==0){
+function generate_pairs(ctx, members = []) {
+    if(members%2!==0||members.length===0){
         return
     }
     const sortedMembers = members.sort(
@@ -61,6 +61,10 @@ function generate_pairs(members = []) {
         }
     }
 
+    if(members.length===sortedMembers.length){
+        ctx.reply(`Неможливо згенерувати нові унікальні пари =(`)
+        return;
+    }
     //for pairs that already meet
     while (meetBeforeUsers.length > 0) {
         let user1 = meetBeforeUsers.pop()
@@ -113,7 +117,7 @@ bot.command('speed_dating', (ctx) => {
 				ctx.reply("Реєстрація завершена! Генеруємо пари...")
 				console.log(participants.length)
 				reg_status = false
-				generate_pairs(participants)
+				generate_pairs(ctx, participants)
 
 			} else {
 				if (participants.length >= 3) {
@@ -161,7 +165,7 @@ bot.command('go', (ctx) => {
 		  			console.log(participants)
 		  			if (extend_reg) {
 		  				extend_reg = false
-		  				generate_pairs(participants)
+		  				generate_pairs(ctx, participants)
 		  			}
 		  		},
 		  		function(error) {
